448c4ad3050fcba9f50f0a86afd117627202ddc3
Matt Miller <Matt.Miller2@albertsons.com>
2023-09-05 15:44:11 -0500

    Created unit test.

---
+++ src/test/java/com/albertsons/person/PersonServiceTest.java
@@ -0,0 +1,11 @@
+package com.albertsons.person;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class PersonServiceTest {
+
+    @Test
+    void createPerson() {
+    }
+}
