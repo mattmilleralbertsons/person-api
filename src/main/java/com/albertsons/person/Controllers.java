dd96ff7edd236c52d1b43716c2c183991c144e89
Matt Miller <Matt.Miller2@albertsons.com>
2023-12-22 09:54:51 -0600

    Checkpoint.

--- src/main/java/com/albertsons/person/Controllers.java
+++ src/main/java/com/albertsons/person/Controllers.java
@@ -6,13 +6,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/people")
 @Slf4j
 public class Controllers {
 
@@ -22,7 +20,7 @@ public class Controllers {
         this.personService = personService;
     }
 
-    @GetMapping("/{personId}")
+    @GetMapping("/people/{personId}")
     public ResponseEntity<Person> getPerson(@PathVariable("personId") String personId) {
         log.info("Getting person. personId={}", personId);
         Person person = personService.getPerson(UUID.fromString(personId));
@@ -30,7 +28,7 @@ public class Controllers {
         return ResponseEntity.ok(person);
     }
 
-    @PostMapping
+    @PostMapping("/people")
     public ResponseEntity<Person> postPerson(@RequestBody Person person) {
         Person created = personService.createPerson(person);
 
